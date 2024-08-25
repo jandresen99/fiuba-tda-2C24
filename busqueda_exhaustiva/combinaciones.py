4226f90df6f29d236d4c6c29e247c500a8143f4b
Joaquin Andresen <joaquin_andresen@icloud.com>
2024-08-25 17:41:18 -0300

    Agrego comentarios

--- busqueda_exhaustiva/combinaciones.py
+++ busqueda_exhaustiva/combinaciones.py
@@ -1,13 +1,16 @@
 def incrementar(c, m):
     j = 0
 
+    # Si los contiguos son consecutivos, incrementamos j
     while c[j]+1 == c[j+1]:
         c[j] = j + 1
         j += 1
     
+    # Si j+1 > m, terminamos
     if j+1 > m:
         return 'fin'
     
+    # Si no, incrementamos c[j]
     c[j] += 1
     return c
 
